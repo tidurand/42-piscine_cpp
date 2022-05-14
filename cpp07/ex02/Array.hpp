baf9dcdfe886c23bacbadf18b5743475109b68eb
Tiphaine DURAND <tidurand@e2r12p5.clusters.42paris.fr>
2022-05-14 12:25:35 +0200

    ok

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,25 +6,45 @@
 /*   By: tidurand <tidurand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 12:49:29 by tidurand          #+#    #+#             */
-/*   Updated: 2022/05/13 16:13:18 by tidurand         ###   ########.fr       */
+/*   Updated: 2022/05/14 12:22:31 by tidurand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+
 
 template <class T>
 class Array
 {
 	public :
 		Array(void): _size(0){};
-		Array(int n): _array(new T[n]), _size(n){};
-		Array(const Array<T> & src):_array(new T[src._size]), _size(src._size){};
-		~Array(){delete [size]};
+		Array(int n)
+		{
+			_size = n;
+			_array = new T[n];
+		};
+		Array(const Array<T> & src):_size(src._size), _array(new T[src._size])
+		{
+			for (int i = 0; i < _size; i++)
+				_array[i] = src._array[i];
+		};
+		~Array(){delete [] _array;};
 		
-		Array<T> &operator=(const Array<T> & rhs);
-		Array<T> operator[](const int);
+		Array<T> &operator=(const Array<T> & rhs)
+		{
+			_size = rhs._size;
+			for (int i = 0; i < _size; i++)
+				_array[i] = rhs._array[i];
+			return (*this);
+		};
+		Array<T> operator[](const int i)
+		{
+			if (i < 0 || i >= _size)
+				throw BadIndex();
+			return _array[i];
+		};
 		
-		int	size(void);
+		int	size(void){return _size;};
 		
 		class BadIndex : public std::exception
 		{
@@ -32,7 +52,15 @@ class Array
 				virtual const char* what() const throw()
 				{return ("Error :Bad Index");}
 		};
+		
 	private :
-		_size;
-		_*array
-}
+		int _size;
+		T *_array;
+};
+
+// template <class T>
+// std::ostream & operator<<(std::ostream& o, const Array<T>& rhs)
+// {
+// 	o << rhs[0];
+// 	return o;
+// }
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,13 +6,23 @@
 /*   By: tidurand <tidurand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 12:48:47 by tidurand          #+#    #+#             */
-/*   Updated: 2022/05/13 12:49:19 by tidurand         ###   ########.fr       */
+/*   Updated: 2022/05/14 12:20:55 by tidurand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
+#include <cstdlib>
 
-int main()
+int main(int, char**)
 {
-	return 0;
+    Array<int> numbers(10);
+	// srand(time(NULL));
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     int value = rand();
+    //     numbers[i] = value;
+    // }
+	numbers[0] = 100;
+	std::cout << numbers.size() << std::endl;
+    return 0;
 }
