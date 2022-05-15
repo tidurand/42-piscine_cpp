43feb74dbd7fc787499bcf4a45771d0184866284
Tiphaine DURAND <tidurand@e1r11p6.clusters.42paris.fr>
2022-05-15 17:19:22 +0200

    ok

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tidurand <tidurand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/15 16:58:54 by tidurand          #+#    #+#             */
-/*   Updated: 2022/05/15 17:03:49 by tidurand         ###   ########.fr       */
+/*   Updated: 2022/05/15 17:18:17 by tidurand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,26 @@
 
 int main()
 {
-MutantStack<int> mstack;
-mstack.push(5);
-mstack.push(17);
-std::cout << mstack.top() << std::endl;
-mstack.pop();
-std::cout << mstack.size() << std::endl;
-mstack.push(3);
-mstack.push(5);
-mstack.push(737);
-mstack.push(0);
-MutantStack<int>::iterator it = mstack.begin();
-MutantStack<int>::iterator ite = mstack.end();
-++it;
---it;
-while (it != ite)
-{
-std::cout << *it << std::endl;
-++it;
-}
-std::stack<int> s(mstack);
-return 0;
+	//FAIRE UN VRAI MAIN
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+	std::cout << *it << std::endl;
+	++it;
+	}
+	std::stack<int> s(mstack);
+	return 0;
 }
